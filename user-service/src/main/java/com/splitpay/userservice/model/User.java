7c7cc47c4afb0e880188f644d45ba1ca72bf1acb
Bhavya Aggarwal <baggarwalb@gmail.com>
2025-12-02 11:11:56 +0530

    feat: add user entity and repository

---
+++ user-service/src/main/java/com/splitpay/userservice/model/User.java
@@ -0,0 +1,24 @@
+package com.splitpay.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "APP_USERS")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String phoneNumber;
+}
