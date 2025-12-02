d6c7bda116481d6fbc94143734f9e8bc96605c99
Bhavya Aggarwal <baggarwalb@gmail.com>
2025-12-02 18:33:20 +0530

    feat: init expense-service and cross-service kafka communication

---
+++ expense-service/src/test/java/com/splitpay/expenseservice/ExpenseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitpay.expenseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
